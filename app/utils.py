45d93aacd4f646bc698e918ed68e51864b038b85
Maximiliano Opitz <mopitz199@gmail.com>
2022-11-28 19:19:29 -0300

    adding typings

--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,15 @@
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import Optional, Tuple
 
 
 class Utils:
     @classmethod
-    def str_to_date(cls, string) -> datetime:
+    def str_to_date(cls, string: str) -> datetime:
         """Method to transform an string to a datetime object"""
         return datetime.strptime(string, "%Y-%m-%d").date()
 
     @classmethod
-    def str_to_datetime(cls, date_str, time_str) -> datetime:
+    def str_to_datetime(cls, date_str: str, time_str: str) -> datetime:
         """Method to transform an string to a datetime object"""
         datetime_str = f"{date_str} {time_str}"
         return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
@@ -17,15 +17,15 @@ class Utils:
     @classmethod
     def has_collision(
         cls,
-        schedule1,
-        schedule2,
+        schedule1: Tuple[datetime, datetime],
+        schedule2: Tuple[datetime, datetime],
     ) -> bool:
         """Method to get if two schedule, has collisions"""
         return schedule1[1] >= schedule2[0] and schedule1[0] <= schedule2[1]
 
     @classmethod
     def get_day_number_from_date(
-        cls, workshift_person_range, date_obj, total_days
+        cls, workshift_person_range: dict, date_obj: datetime.date, total_days: int
     ) -> Optional[int]:
         """Method to get the number of the day in thw workshift from the date object"""
 
@@ -45,7 +45,9 @@ class Utils:
             return None
 
     @classmethod
-    def get_day_schedule(cls, wpr_info, date_obj):
+    def get_day_schedule(cls, wpr_info, date_obj) -> Tuple[(datetime, datetime, bool)]:
+        """Method to get the schedule of an specific date object
+        from the workshift of a person"""
         day_number = cls.get_day_number_from_date(
             workshift_person_range=wpr_info.workshift_person_range,
             date_obj=date_obj,
